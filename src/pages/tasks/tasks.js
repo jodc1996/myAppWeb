1453904f1376cf2e8368544744753c342125c2b8
jodc1996 <josephdurancabrera4@gmail.com>
2023-02-13 19:03:34 +0100

    edit7

--- src/pages/tasks/tasks.js
+++ src/pages/tasks/tasks.js
@@ -38,41 +38,62 @@ async function postData(url = '', data = {}) {
 //     console.log(data); // JSON data parsed by `data.json()` call
 //   });
 
+// function fetchFact(type) {
+//   const inibody = {
+//     "FuncName": "Tai.Backend.Qplant",
+//     "ActiveActor": "WKU11",
+//     "FuncParam02": "SPAIN",
+//     "FuncParam03": "",
+//     "FuncParam04": "",
+//     "FuncParam05": "",
+//     "TraceLog": "Y", 
+//     "FuncParam01": "OEEMONITOR2",
+// }
+// }
+
 
 export default function Task() {
 
 
   const [characters, setCharacters] = useState([])
 
-  const initiaurl = `https://63ea67dbceb46e005c73d32b--neon-pie-587ff5.netlify.app/#/tasks/api/${type}`
+  // const initiaurl = `https://63ea67dbceb46e005c73d32b--neon-pie-587ff5.netlify.app/#/tasks/api/${type}`
   // const initiaurl = `http://intranet.taionline.net:14036/api/loadtfunction`
-  const inibody = {
-    "FuncName": "Tai.Backend.Qplant",
-    "ActiveActor": "WKU11",
-    "FuncParam02": "SPAIN",
-    "FuncParam03": "",
-    "FuncParam04": "",
-    "FuncParam05": "",
-    "TraceLog": "Y", 
-    "FuncParam01": "OEEMONITOR2",
-}
+//   const inibody = {
+//     "FuncName": "Tai.Backend.Qplant",
+//     "ActiveActor": "WKU11",
+//     "FuncParam02": "SPAIN",
+//     "FuncParam03": "",
+//     "FuncParam04": "",
+//     "FuncParam05": "",
+//     "TraceLog": "Y", 
+//     "FuncParam01": "OEEMONITOR2",
+// }
 
-  const fetchChraracter = (url, body) => {
+  const fetchChraracter = (type) => {
     // console.log(`${url} : ${body}`)
-    postData(url, body)
-      .then(data => {setCharacters(data.Payload); // JSON data parsed by `data.json()` call
-      })
-      .catch(rejected => {
-        console.log(rejected);
-    });
+    // postData(url, body)
+    // .then((res) => res.text())
+    //   .then(data => {setCharacters(data.Payload); // JSON data parsed by `data.json()` call
+    //   })
+    //   .catch(rejected => {
+    //     console.log(rejected);
+    // });
+
+    fetch(`https://63ea67dbceb46e005c73d32b--neon-pie-587ff5.netlify.app/#/tasks/api/${type}`, inibody)
+  .then((res) => res.text())
+  .then((data) => setCharacters(data.Payload))
+  .catch((error) => console.log(error))
 
   }
    useEffect(() => {
-    console.log(fetchChraracter(initiaurl, inibody))
-    fetchChraracter(initiaurl, inibody)
+    // console.log(fetchChraracter(initiaurl, inibody))
+    fetchChraracter(type)
 
 
    }, []);
+   
+   
 
   return (
     <React.Fragment>
